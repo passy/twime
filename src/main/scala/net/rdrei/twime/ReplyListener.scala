c2a74c9b7271852154cd9e34550297b90e5f1986
Pascal Hartig <passy@twitter.com>
2013-10-08 22:44:37 +0100

    Use actual replies

--- src/main/scala/net/rdrei/twime/ReplyListener.scala
+++ src/main/scala/net/rdrei/twime/ReplyListener.scala
@@ -1,13 +1,22 @@
 package net.rdrei.twime
 
-import twitter4j.Status
+import twitter4j.{StatusUpdate, Status}
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 
 class ReplyListener extends AbstractReplyListener {
   def onStatus(status: Status): Unit = {
+    if (!isInteresting(status))
+      return
+
     val screenName = status.getUser.getScreenName
     val time = DateTime.now.toString(DateTimeFormat.forPattern("d.M.y H:m:s"))
-    T4JClient().updateStatus(s"@$screenName The current time is $time.")
+    val update = new StatusUpdate(s"@$screenName The current time is $time.")
+    update.setInReplyToStatusId(status.getId)
+
+    T4JClient().updateStatus(update)
   }
+
+  private def isInteresting(status: Status): Boolean =
+    status.getUser.getScreenName != "twimebot"
 }
